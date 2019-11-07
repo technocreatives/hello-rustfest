ae66772dd72cce14ac2cbcad85b6e6b808f47fc0
Byggare Bob <bob@technocreatives.com>
2019-11-07 13:17:27 +0000

    Generated the docs again

---
+++ docs/hello_rustfest/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Role","The dev jobs we are currently hiring for."]],"fn":[["apply_for_a_job","Join us!"],["hire_us","Does your company want to get started with Rust and/or needs some additional people to get going quickly?"]]});
